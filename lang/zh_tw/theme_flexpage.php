003961b5e3cc48f702eb3ded28541718d34f2b2c
David Castro <david.castro@blackboard.com>
2018-04-23 15:43:20 +0000

    INT-13192: Importing zh_tw lang

---
+++ lang/zh_tw/theme_flexpage.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Flexpage 主題';
+$string['region-side-post'] = '右側';
+$string['region-side-pre'] = '左側';
+$string['region-side-top'] = '頂端';
+$string['region-main'] = '主區域';
+$string['choosereadme'] = '此主題提供 Flexpage 課程格式的呈現方式。';
